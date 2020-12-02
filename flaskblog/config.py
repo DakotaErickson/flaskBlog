4188d1b035dda1da372aa187b0abf160d0b29b8c
Dakota Erickson <derickson29@gmail.com>
2020-12-02 10:06:37 -0600

    updating config file

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -10,5 +10,5 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = config.get('EMAIL')
+    MAIL_USERNAME = config.get('EMAILUSER')
     MAIL_PASSWORD = config.get('EMAILPASS')
